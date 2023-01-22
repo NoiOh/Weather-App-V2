06a86f331ef01236a50abdbff1e3ae27f15b1985
Noi Houangvan <45153151+NoiOh@users.noreply.github.com>
2023-01-21 21:50:46 -0500

    added date and time in JS

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+//Date & Time
+function formatDate(date) {
+  //let date = new Date(date);
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 // Display Weather
 function showWeather(response) {
   console.log(response);
@@ -17,7 +46,6 @@ function showWeather(response) {
   document.querySelector("#wind").innerHTML = `${Math.round(
     response.data.wind.speed
   )} mp/h`;
-
   iconElement.setAttribute(
     "src",
     `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
@@ -30,6 +58,11 @@ let key = "ba9b6442401a4fb7923cdf0adatc4bob";
 let units = "imperial";
 let url = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${key}&units=${units}`;
 axios.get(url).then(showWeather);
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+
+dateElement.innerHTML = formatDate(currentTime);
 //}
 
 //Search City
